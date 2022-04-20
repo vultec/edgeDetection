3b9f2e57548a59dd166d681e8358c376678e83b1
vultec <104072008+vultec@users.noreply.github.com>
2022-04-20 13:11:16 +0100

    Update videoCapture.py

--- videoCapture.py
+++ videoCapture.py
@@ -1,7 +1,42 @@
-# import the opencv library
 import cv2
 import numpy as np
 import time
+from matplotlib import pyplot as plt
+from numba import jit
+
+
+
+@jit(parallel = True)
+def pythagoras(horizontal,vertical,h,w):
+        h_m = (horizontal[:,:,0] + horizontal[:,:,1] + horizontal[:,:,2] ) / 3
+        h_m = h_m.reshape((h,w,1)) / 255
+        v_m = (vertical[:,:,0] + vertical[:,:,1] + vertical[:,:,2] ) /3
+        v_m = v_m.reshape((h,w,1)) / 255
+        diagonal = np.sqrt(np.square(h_m)+np.square(v_m))
+        return diagonal
+
+height = 1080
+width = 1920
+
+a = []
+b = []
+for i in range(1):
+        a.append(np.random.rand(height,width,3)*255)
+        b.append(np.random.rand(height,width,3)*255)
+
+
+times = [time.time()]
+for i in range(1):
+        pythagoras(a[i],b[i],height,width)
+        times.append(time.time())
+times = np.array(times)
+times = times[1:] - times[:-1]
+print(times)
+
+
+
+
+
 
 vid = cv2.VideoCapture(0)
 
@@ -22,9 +57,11 @@ while(True):
 	horizontal = (cv2.Sobel(frame, cv2.CV_32F, 1, 0, ksize= - 1) + cv2.Sobel(frame, cv2.CV_32F, 1, 0, ksize= 3)) / 2
 	vertical = (cv2.Sobel(frame, cv2.CV_32F, 0, 1, ksize= - 1) + cv2.Sobel(frame, cv2.CV_32F, 0, 1, ksize= 3)) / 2
 
-	h_m = np.mean(horizontal,axis=2).reshape((h,w,1)) / 255
-	v_m = np.mean(vertical,axis=2).reshape((h,w,1)) / 255
-	sobel = np.sqrt(np.square(h_m)+np.square(v_m))
+	#h_m = np.mean(horizontal,axis=2).reshape((h,w,1)) / 255
+	#v_m = np.mean(vertical,axis=2).reshape((h,w,1)) / 255
+	#sobel = np.sqrt(np.square(h_m)+np.square(v_m))
+
+	sobel = pythagoras(horizontal,vertical,h,w)
 
 	# Display
 	cv2.imshow('sobel', sobel)
